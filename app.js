799e5e84deca1bc9f99b3ddb91a9addadccfd28c
Shahrukh-Rehman <shahrukhrehman321@gmail.com>
2023-09-09 19:17:05 +0500

    Question 3

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 // Chapter 01 Question 1 & 2
-alert("Error! Please Enter a Valid Password")
+// alert("Error! Please Enter a Valid Password")
 
+// Chapter 01 Question 3
+alert("Welcome to Js Land... \nHappy Coding!")
