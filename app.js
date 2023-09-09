a5b135b9c5c4b18ea707c3032c5ece6f068091fb
Shahrukh-Rehman <shahrukhrehman321@gmail.com>
2023-09-09 23:34:20 +0500

    Question 4

--- app.js
+++ app.js
@@ -2,4 +2,9 @@
 // alert("Error! Please Enter a Valid Password")
 
 // Chapter 01 Question 3
-alert("Welcome to Js Land... \nHappy Coding!")
+// alert("Welcome to Js Land... \nHappy Coding!")
+
+// Chapter 01 Question 4
+// alert("Welcome to Js Land...")
+// var checkBox = ("checkbox").outerHTML
+// alert("Happy Coding!\nPreventing this page for creating additional dialogs")
